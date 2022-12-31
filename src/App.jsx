ebee725c46113888e95697ae61d2522badd7747b
Danss <kualfian123@gmail.com>
2022-12-31 15:41:58 +0700

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
-import Home from './components/Home'
-import Dashboard from './components/Dashboard'
 import { Route, Routes} from 'react-router-dom'
-import Login from './components/Login'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='*' element={<Home/>}/>
-      <Route path='/dashboard' element={<Dashboard/>}/>
-      <Route path='/login' element={<Login/>}/>
     </Routes>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/index.css'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
-import { GoogleOAuthProvider } from '@react-oauth/google'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
-      <GoogleOAuthProvider clientId='453345107768-62oamer07figpo2hbe2un0sulnp5s7cb.apps.googleusercontent.com'>
-        <App />
-      </GoogleOAuthProvider>
+      <App />
     </HashRouter>
   </React.StrictMode>
 )
